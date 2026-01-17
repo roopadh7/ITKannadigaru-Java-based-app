2fac975ae9e44effcf8463e5d08259d81ee79491
Manoj-K <k_manoj@apple.com>
2026-01-17 16:42:33 +0530

    updated the monitoring dahsbord creation

--- scripts/grafana.sh
+++ scripts/grafana.sh
@@ -4,4 +4,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm install monitoring prometheus-community/kube-prometheus-stack -n monitoring
 kubectl get secret monitoring-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 --decode
+kubectl patch svc monitoring-grafana -n monitoring  -p '{"spec": {"type": "LoadBalancer"}}'
 
+## TO get secret
+kubectl get secret monitoring-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 --decode
